60b54c327965b09e4df624a19ae5f3bdd2264b37
xiongyuParuru <11781827@qq.com>
2014-10-25 21:25:00 +0800

    first

---
+++ src/com/vmingtang/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vmingtang;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	Button btnLogin;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btnLogin = (Button)findViewById(R.id.btnLogin);
+	}
+
+}
