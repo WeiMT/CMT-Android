0daba7b6d3ce37d8c60915b2c8533d4e91f1af5f
xiongyuParuru <11781827@qq.com>
2014-10-25 21:32:04 +0800

    third

--- src/com/vmingtang/MainActivity.java
+++ src/com/vmingtang/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		btnLogin = (Button)findViewById(R.id.btnLogin);
+		// 123
 		
 	}
 
